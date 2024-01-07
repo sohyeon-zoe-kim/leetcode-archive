696491a94e19d0269a4fde0c4644c334262383cf
sohyeon-zoe-kim <hlna030838@gmail.com>
2024-01-07 10:03:21 +0900

    feat: Array101 - Squares of a Sorted Array (solve it in O(N) time and O(N) spaces)

--- Arrays101/squares-of-a-sorted-array.js
+++ Arrays101/squares-of-a-sorted-array.js
@@ -2,18 +2,39 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const sortedSquares = nums => {
-  const result = nums
-    .map(num => {
-      return num * num
-    })
-    .sort((a, b) => {
-      return a - b
-    })
+// const sortedSquares = nums => {
+//   const result = nums
+//     .map(num => {
+//       return num * num
+//     })
+//     .sort((a, b) => {
+//       return a - b
+//     })
 
-  return result
-}
-const example = [-7, -3, 2, 3, 11]
-const test = sortedSquares(example)
+//   return result
+// }
 
-console.log(test)
+/** solve in O(N) time and O(N) space */
+const sortedSquares = (nums) => {
+  const result = new Array(nums.length);
+  let left = 0;
+  let right = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+      result[i] = nums[right] ** 2;
+      right--;
+    } else {
+      result[i] = nums[left] ** 2;
+      left++;
+    }
+  }
+
+  return result;
+};
+
+const example = [-4, -1, 0, 3, 10];
+
+const test = sortedSquares(example);
+
+console.log(test);
